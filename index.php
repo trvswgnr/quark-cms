ddee725821e678449d882a8b0c68d6af8950da1a
trav <trvswgnr@gmail.com>
2019-04-04 02:06:29 -0400

    update

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
-<?php require 'connection.php'; ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Food @ TravisAW.com</title>
-</head>
-<body>
+<?php require 'header.php'; ?>
 	<h1>Welcome!</h1>
+	
 	<?php
 	function list_file_links( $ext = '*' ) {
 
@@ -16,7 +8,7 @@
 
 		echo '<ul>' . implode( '', array_map( 'sprintf', array_fill( 0, count( $files ), '<li><a href="%s">%s</a></li>' ), $files, $files ) ) . '</ul>';
 	}
+	
 	list_file_links( 'php' );
 	?>
-</body>
-</html>
+<?php require 'footer'; ?>
