51c39d96fba2554c088726a094cd734f6854cfc0
trav <trvswgnr@gmail.com>
2019-04-04 02:09:34 -0400

    add 'add column' to table feature

--- setup.php
+++ setup.php
@@ -8,11 +8,42 @@
 // Import Setup class.
 require 'class-setup.php';
 
+require 'header.php';
+
 if ( filter_input( INPUT_POST, 'submit', FILTER_SANITIZE_SPECIAL_CHARS ) ) {
 	$setup = new Setup( $conn );
 }
+
+if ($_POST['submit_table']) {
+	try {
+		$table = 'posts';
+		$column = $_POST['column_name'] ?: die();
+		$data_type = $_POST['data_type'];
+		$sql   = "ALTER TABLE $table
+			ADD $column $data_type NOT NULL;";
+
+		$conn->exec( $sql );
+		echo "Added $column successfully.";
+	} catch ( PDOException $e ) {
+		echo "Error Creating $column: " . $e->getMessage();
+	}
+}
 ?>
 
+<h1>Site Setup</h1>
 <form action="" method="post">
 	<input type="submit" name="submit" value="Initialize Site">
 </form>
+
+<h2>Add Column to 'posts'</h2>
+<form action="" method="post">
+	<label for="column_name">Column Name</label>
+	<input type="text" name="column_name">
+
+	<label for="data_type">Data Type</label>
+	<input type="text" name="data_type" value="text">
+
+	<div><input type="submit" name="submit_table" value="Add Column"></div>
+</form>
+
+<?php require 'footer.php'; ?>
