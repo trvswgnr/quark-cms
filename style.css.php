d2cd28131d204d5635c6a23032ae841f64c6fc49
trav <trvswgnr@gmail.com>
2019-04-04 02:09:57 -0400

    pull remote css files and add to css

--- style.css.php
+++ style.css.php
@@ -1,9 +1,20 @@
-<?php header("Content-type: text/css; charset: UTF-8"); ?>
+<?php
+header("Content-type: text/css; charset: UTF-8");
+
+require 'functions.php';
+
+$bootstrap = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
+echo get_remote_file_contents($bootstrap);
+?>
 
 <?php
 $brand_color = '#990000';
 $link_color  = '#555555';
 ?>
+
+h1, h2, h3, h4 {
+	margin-top: 1em;
+}
 
 label {
 	display: block;
