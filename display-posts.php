aeb493a2928e70b3a820b08b9811ebece70de77e
trav <trvswgnr@gmail.com>
2019-04-05 00:59:51 -0400

    replace id with slug

--- display-posts.php
+++ display-posts.php
@@ -9,7 +9,7 @@ $posts = $statement->fetchAll();
 <?php if ( count( $posts ) > 0 ) : ?>
 	<ul>
 	<?php foreach ( $posts as $post ) : ?>
-		<li><a href="<?php echo get_site_directory() . '/single.php?id=' . $post['ID']; ?>"><?php echo $post['title']; ?></a></li>
+		<li><a href="<?php echo get_site_directory() . '/single.php?slug=' . $post['slug']; ?>"><?php echo $post['title']; ?></a></li>
 	<?php endforeach; ?>
 	</ul>
 <?php else : ?>
