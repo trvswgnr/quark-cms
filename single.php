2b13e0ca911e203b501713a1240f05f25d153073
trav <trvswgnr@gmail.com>
2019-04-05 01:01:15 -0400

    show error message if no slug found

--- single.php
+++ single.php
@@ -2,15 +2,16 @@
 require 'header.php';
 
 try {
-	$post_id = $_GET['id'] ?: 1;
-	$sql = "SELECT * FROM posts WHERE ID=$post_id LIMIT 1";
+	$post_slug = $_GET['slug'];
+	$message = $post_slug == '' ? 'no slug found' : false; 
+	$sql = "SELECT * FROM posts WHERE slug='$post_slug' LIMIT 1";
 	$post = $conn->query($sql)->fetch();
 } catch ( PDOException $e ) {
 	echo 'Error Getting Post: ' . $e->getMessage();
 }
 ?>
 
-<h1><?php echo $post['title']; ?></h1>
+<h1><?php echo $message ?: $post['title']; ?></h1>
 <p><small>Published: <?php echo $post['date']; ?></small></p>
 <p><small>Modified: <?php echo $post['modified']; ?></small></p>
 <div><?php echo $post['content']; ?></div>
