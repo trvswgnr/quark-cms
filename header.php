e6380c94d30bd21b0f0f5939672733a766bcc3a7
trav <trvswgnr@gmail.com>
2019-04-05 01:00:49 -0400

    use site directory function to get absolute path

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Create Post</title>
-	<link rel='stylesheet' type='text/css' href='style.css.php' />
+	<link rel='stylesheet' type='text/css' href='<?php site_directory(); ?>/style.css.php' />
 
 </head>
 <body>
