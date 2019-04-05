e23d6e7f6970d8307522c73981d40f8fb0654b2c
trav <trvswgnr@gmail.com>
2019-04-05 01:28:04 -0400

    normalize and add base styles

--- style.css.php
+++ style.css.php
@@ -3,14 +3,23 @@ header("Content-type: text/css; charset: UTF-8");
 
 require 'functions.php';
 
-$bootstrap = 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
-echo get_remote_file_contents($bootstrap);
+$normalize = 'https://necolas.github.io/normalize.css/8.0.1/normalize.css';
+echo remote_file_contents( $normalize );
 ?>
 
 <?php
 $brand_color = '#990000';
 $link_color  = '#555555';
 ?>
+body {
+	margin: 1em;
+	font-family: 'Helvetica Neue', arial, sans-serif;
+}
+
+.container {
+	max-width: 768px;
+	margin: 0 auto;
+}
 
 h1, h2, h3, h4 {
 	margin-top: 1em;
