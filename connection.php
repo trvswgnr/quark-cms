9ee0fe4ec73baac54b8659c4d0b5f698d018fed4
trav <trvswgnr@gmail.com>
2019-04-06 19:55:21 -0400

    move credentials to 'credentials.php'

--- connection.php
+++ connection.php
@@ -1,19 +1,11 @@
 <?php
+require 'credentials.php';
 
-function connection() {
-	$host   = 'localhost';
-	$user   = 'root';
-	$pass   = 'root';
-	$dbname = 'quark_cms';
-
-	try {
-		$conn = new PDO( "mysql:host=$host;", $user, $pass );
-		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-		$conn->exec( "CREATE DATABASE IF NOT EXISTS $dbname; USE $dbname;" );
-		return $conn;
-	} catch ( PDOException $e ) {
-		echo 'Connection failed: ' . $e->getMessage();
-	}
+try {
+	$conn = new PDO( "mysql:host=$host;", $user, $pass );
+	$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	$conn->exec( "CREATE DATABASE IF NOT EXISTS $dbname; USE $dbname;" );
+	echo '<script>console.log("Connected Successfully!");</script>';
+} catch ( PDOException $e ) {
+	echo 'Connection failed: ' . $e->getMessage();
 }
-
-$conn = connection();
