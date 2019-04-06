22ea81f57a08fa5fcb05e9b0700609b9efa05708
trav <trvswgnr@gmail.com>
2019-04-06 12:48:00 -0400

    put connection in function so less variables are exposed

--- connection.php
+++ connection.php
@@ -1,8 +1,18 @@
 <?php
-$host   = 'localhost';
-$user   = 'root';
-$pass   = 'root';
-$dbname = 'food_taw';
 
-$conn = new PDO( "mysql:host=$host;dbname=$dbname", $user, $pass );
-$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+function connection() {
+	$host   = 'localhost';
+	$user   = 'root';
+	$pass   = 'root';
+	$dbname = 'quark_cms';
+	try {
+		$conn = new PDO( "mysql:host=$host;", $user, $pass );
+		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		$conn->exec( "CREATE DATABASE IF NOT EXISTS $dbname; USE $dbname;" );
+		return $conn;
+	} catch ( PDOException $e ) {
+		echo 'Connection failed: ' . $e->getMessage();
+	}
+}
+
+$conn = connection();
