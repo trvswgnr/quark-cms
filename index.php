ba76fdd562eb9433124e9201dfc43eb778ea4aab
trav <trvswgnr@gmail.com>
2019-04-06 13:32:11 -0400

    add meta information

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+/**
+ * Name: Quark CMS
+ * Description: A fast, basic CMS for building and maintaining websites.
+ * Author: Travis Wagner
+ * Author URI: https://travisaw.com
+ * Version: 0.1.0-alpha
+ *
+ * @package quark
+ */
+
+?>
+
 <?php require 'header.php'; ?>
 <h1>Welcome!</h1>
 <p>This is the home page. Currently it can only be edited with a text editor, which isn't desirable.</p>
