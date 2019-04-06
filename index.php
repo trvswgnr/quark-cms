6cf6eaad30e9faea5136083d8c0c33a6cf4dd8bb
trav <trvswgnr@gmail.com>
2019-04-06 13:18:57 -0400

    move temporary navigation to footer

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php require 'header.php'; ?>
-	<h1>Welcome!</h1>
-	
-	<?php
-	function list_file_links( $ext = '*' ) {
-
-		$files = glob( "*.$ext" );
-
-		echo '<ul>' . implode( '', array_map( 'sprintf', array_fill( 0, count( $files ), '<li><a href="%s">%s</a></li>' ), $files, $files ) ) . '</ul>';
-	}
-	
-	list_file_links( 'php' );
-	?>
-<?php require 'footer'; ?>
+<h1>Welcome!</h1>
+<p>This is the home page. Currently it can only be edited with a text editor, which isn't desirable.</p>
+<?php require 'footer.php'; ?>
