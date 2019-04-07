88a20b8f9a74e64841521ff35e19a56e2ffe2282
trav <trvswgnr@gmail.com>
2019-04-07 19:42:19 -0400

    check if users table exists and redirect to install.php if not

--- quark-head.php
+++ quark-head.php
@@ -20,21 +20,23 @@ require 'security.php';
 // import global functions.
 require 'functions.php';
 
+quark_debug( constant( 'DEBUG' ) );
+
+if ( ! is_current_file( 'install.php' ) ) {
+	require 'connection.php';
+	try {
+		$users_table_exists = $conn->query( 'select 1 from `users` LIMIT 1' );
+	} catch ( PDOException $e ) {
+		redirect( 'install.php' );
+	}
+}
+
 if ( ! is_logged_in() && ! is_current_file( 'login.php' ) && ! is_current_file( 'install.php' ) ) {
-	$redirect_url = get_site_url() . 'login.php';
-	header( "Location: $redirect_url", true, 303 );
-	die();
+	redirect( 'login.php' );
 }
 
 if ( filter_get( 'installed' ) === 'true' ) {
 	echo rename( ABSPATH . 'install.php', ABSPATH . 'install.bak' ) ? '' : 'error renaming';
 }
 
-quark_debug( constant( 'DEBUG' ) );
-
-if ( ! is_current_file( 'install.php' ) ) {
-	require 'connection.php';
-}
-
 date_default_timezone_set( constant( 'TIMEZONE' ) );
-
