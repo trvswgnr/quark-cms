bafe8060fcba6b487c0f68dc522866f0256aff96
trav <trvswgnr@gmail.com>
2019-04-07 17:41:16 -0400

    add redirect to login after submit, move to class

--- install.php
+++ install.php
@@ -13,9 +13,6 @@ require 'class-setup.php';
 
 if ( secure_input( 'submit' ) ) {
 	$setup = new Setup();
-	$redirect_url = get_site_url() . 'index.php';
-	header( "Location: $redirect_url", true, 303 );
-	die();
 }
 
 /**
@@ -42,24 +39,38 @@ function create_column() {
 if ( isset( $_POST['submit_table'] ) ) {
 	create_column();
 }
+
 ?>
 
 <h1>Site Setup</h1>
 <form action="" method="post">
 	<h2>Database Information:</h2>
-	<label for="host">Host Name</label>
+	<label for="host">Database Host Name</label>
 	<input type="text" name="host" value="localhost">
 
 	<label for="dbname">Database Name</label>
 	<input type="text" name="dbname">
 
-	<label for="user">Username</label>
+	<label for="user">Database Username</label>
 	<input type="text" name="user">
 
-	<label for="pass">Password</label>
+	<label for="pass">Database Password</label>
 	<input type="text" name="pass">
 
-	<div>
+	<h2>Create Admin</h2>
+	<div class="input-group">
+		<label>Username</label>
+		<input type="text" name="admin_user" value="">
+	</div>
+	<div class="input-group">
+		<label>Email</label>
+		<input type="email" name="admin_email" value="">
+	</div>
+	<div class="input-group">
+		<label>Password</label>
+		<input type="password" name="admin_password">
+	</div>
+	<div class="input-group">
 		<input type="submit" name="submit" value="Initialize Site">
 	</div>
 </form>
