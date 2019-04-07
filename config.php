2bfc46464db799247a210a1263f230d663d1dccf
trav <trvswgnr@gmail.com>
2019-04-07 02:27:49 -0400

    set debugging to true

--- config.php
+++ config.php
@@ -3,5 +3,5 @@ define( 'DB_HOST', 'localhost' );
 define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'root' );
 define( 'DB_NAME', 'quark_cms' );
-define( 'DEBUG', false );
+define( 'DEBUG', true );
 define( 'TIMEZONE', 'America/New_York' );
