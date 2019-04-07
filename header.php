28392941c8553780f49cb4cca5c5542a0e733e41
trav <trvswgnr@gmail.com>
2019-04-06 20:05:04 -0400

    change <title>

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Create Post</title>
-	<link rel='stylesheet' type='text/css' href='<?php site_directory(); ?>/style.css.php' />
+	<title>Quark Admin</title>
+	<link rel="stylesheet" type="text/css" href="<?php site_url(); ?>css/style.css.php" />
 
 </head>
 <body>
