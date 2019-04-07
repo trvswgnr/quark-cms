cedb333b16952a5e02b0caa87603f341cd19ad63
trav <trvswgnr@gmail.com>
2019-04-06 20:05:24 -0400

    add sanitize_html function using htmlpurifier

--- security.php
+++ security.php
@@ -6,7 +6,7 @@
  */
 
 /**
- * Secure Attribute
+ * Secure Input
  *
  * @param string $field The 'name' value of the form input.
  * @param string $type (Optional) The type of form method. Either 'post' or 'get'.
@@ -15,3 +15,12 @@ function secure_input( $field, $type = 'post' ) {
 	$type = ( 'get' === strtolower( $type ) ) ? INPUT_GET : INPUT_POST;
 	return filter_input( $type, $field, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES );
 }
+
+function sanitize_html( $html ) {
+	$purifier_config = HTMLPurifier_Config::createDefault();
+	$html_purifier = new HTMLPurifier( $purifier_config );
+	$clean_html = $html_purifier->purify( $html );
+	return $clean_html;
+}
+
+function is_admin () {}
