030e4c3a578b1e29e0ec7daf5b8255074a56c229
trav <trvswgnr@gmail.com>
2019-04-08 00:31:00 -0400

    update connection.php

--- connection.php
+++ connection.php
@@ -11,15 +11,14 @@
  * @return PDO $conn Connection object.
  */
 function connection() {
-	$host   = constant( 'DB_HOST' );
-	$user   = constant( 'DB_USER' );
-	$pass   = constant( 'DB_PASSWORD' );
-	$dbname = constant( 'DB_NAME' );
+	$host   = DB_HOST;
+	$user   = DB_USER;
+	$pass   = DB_PASSWORD;
+	$dbname = DB_NAME;
 
 	try {
 		$conn = new PDO( "mysql:host=$host; dbname=$dbname", $user, $pass );
 		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-		// $conn->exec( "CREATE DATABASE IF NOT EXISTS $dbname; USE $dbname;" );
 		echo '<script>console.log("Connected Successfully!");</script>';
 		return $conn;
 	} catch ( PDOException $e ) {
