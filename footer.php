dda352518ea5087b48b63ca6ca38eca13647d403
trav <trvswgnr@gmail.com>
2019-04-07 20:31:57 -0400

    add better temporary navigation

--- footer.php
+++ footer.php
@@ -2,7 +2,12 @@
 if ( is_logged_in() ) :
 	?>
 	<h2>Temporary Navigation</h2>
-	<?php list_file_links( 'php' ); ?>
+	<ul>
+		<li><a href="<?php site_url(); ?>">Quark Home</a></li>
+		<li><a href="<?php site_url(); ?>display-posts.php">View Posts</a></li>
+		<li><a href="<?php site_url(); ?>create-post.php">Create Post</a></li>
+		<li><a href="<?php site_url(); ?>add-user.php">Add User</a></li>
+	</ul>
 	<a href="<?php site_url(); ?>login.php?logout=true">Logout</a>
 	<?php
 endif;
